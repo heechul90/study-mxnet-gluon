613f783dce95e36f5b25ab1ac919ee1f8f0b71e1
Heechul90 <heechul4296@gmail.com>
2020-01-30 12:03:37 +0900

    GoogLeNet with mxnet.gluon for deeplearning

--- googlenet-gluon/main4.py
+++ googlenet-gluon/main4.py
@@ -6,6 +6,27 @@ from mxnet import nd, autograd
 from mxnet import gluon
 from mxnet.gluon import nn
 
+mx.random.seed(1)
+##### 전처리 ##############################################
+ctx = mx.cpu()
+
+def transformer(data, label):
+    data = mx.image.imresize(data, 96, 96)
+    data = mx.nd.transpose(data, (2, 0, 1))
+    data = data.astype(np.float32)
+    return data, label
+
+batch_size = 64
+train_data = gluon.data.DataLoader(
+    gluon.data.vision.MNIST('dataset/data', train = True, transform = transformer),
+    batch_size = batch_size, shuffle = False, last_batch = 'discard')
+
+test_data = gluon.data.DataLoader(
+    gluon.data.vision.MNIST('dataset/data', train = False, transform = transformer),
+    batch_size = batch_size, shuffle = True, last_batch = 'discard')
+
+
+
 dshape = (128, 3, 224, 224)
 nclass = 100
 
@@ -118,5 +139,53 @@ def gluon_hybridblock(n=100, hybridize=True):
 gluon_hybridblock()
 
 
+# train
 
+net = GoogLeNet(10)
+
+net.collect_params().initialize(mx.init.Normal(sigma = 0.05), ctx = ctx)
+
+trainer = gluon.Trainer(net.collect_params(), 'adam', {'learning_rate': 0.001})
+
+
+
+
+# 오차 함수
+softmax_cross_entropy  = gluon.loss.SoftmaxCrossEntropyLoss()
+
+def evaluate_accuracy(data_iterator, net):
+    acc = mx.metric.Accuracy()
+    for d, l in data_iterator:
+        data = d.as_in_context(ctx)
+        label = l.as_in_context(ctx)
+        output = net(data)
+        predictions = nd.argmax(output, axis = 1)
+        acc.update(preds = predictions, labels = label)
+    return acc.get()
+
+
+####### train ######
+epochs = 10
+smoothing_constant = 0.01
+
+for e in range(epochs):
+    for i, (d, l) in enumerate(train_data):
+        data = d.as_in_context(ctx)
+        label = l.as_in_context(ctx)
+        with autograd.record():
+            output = net(data)
+            loss = softmax_cross_entropy(output, label)
+        loss.backward()
+        trainer.step(data.shape[0])
+
+        ############
+        # keep a moving average of the losses
+        ############
+
+        curr_loss = nd.mean(loss).asscalar()
+        moving_loss = (curr_loss if ((i == 0) and (e == 0)) else (1 - smoothing_constant) * moving_loss + (smoothing_constant) * curr_loss)
+
+    test_accuracy = evaluate_accuracy(test_data, net)
+    train_accuracy = evaluate_accuracy(train_data, net)
+    print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, moving_loss, train_accuracy, test_accuracy))
 
